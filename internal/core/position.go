33a82eaf26edaec8d466dd961bc71d1bdad1191e
Calvin Figuereo-Supraner <mail@calvin.page>
2025-11-09 22:03:14 -0800

    Use exported fields to ignore staticcheck lint

--- internal/core/position.go
+++ internal/core/position.go
@@ -2,28 +2,30 @@ package core
 
 // Position describes a position.
 type Position struct {
+	// TODO(clfs): Make these fields unexported.
+
 	// Squares occupied by white pieces.
-	whiteBB Bitboard
+	WhiteBB Bitboard
 
 	// Squares occupied by black pieces.
-	blackBB Bitboard
+	BlackBB Bitboard
 
 	// Piece occupancy bitboards, indexed by [PieceType].
-	pieceBBs [6]Bitboard
+	PieceBBs [6]Bitboard
 
 	// The player whose turn it is.
-	turn Color
+	Turn Color
 
 	// Castling rights.
-	castling Castling
+	Castling Castling
 
 	// The right to capture en passant.
-	enPassant EnPassant
+	EnPassant EnPassant
 
 	// The number of plies since the start of the game.
-	plies uint16
+	Plies uint16
 
 	// The number of plies since the last capture, the last pawn advance, or the
 	// start of the game, whichever is least.
-	fiftyMoveRule uint8
+	FiftyMoveRule uint8
 }
